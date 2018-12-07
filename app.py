fe542f73e39244dd4f8adb00ad9d0cbb48275c9a
kuragen <newfrog98@gmail.com>
2018-12-08 00:43:10 +0700

    ２２

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
+from flaski.models import WikiContent
+from flaski.database import db_session
+from datetime import datetime
+
 
 app = Flask(__name__)
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 @app.route('/')
@@ -11,8 +19,8 @@ def index():
     lis = ["a1", "a2", "a3"]
     dic = {"name":"John", "age":24}
     bl = True
-
-    return render_template('index.html', s=s, lis=lis, dic=dic, bl=bl)
+    contents = WikiContent.query.all()
+    return render_template('index.html', s=s, lis=lis, dic=dic, bl=bl, contents=contents)
 
 
 @app.route('/test', methods=['GET', 'POST'])
@@ -23,6 +31,25 @@ def test():
         res = request.form['post_value']
 
     return res
+
+#/<title>を指定することで、index.htmlのtitle=content.titleを指定している。methods=["GET"]で、GETリクエストを指定。
+@app.route("/<title>", methods=["GET"])
+def show_content(title):
+    """
+    :param title:modelに対するクエリ文字列
+    :return:
+    """
+    # wikicontentテーブルから、titleでフィルタ(where指定して取得) firstは1行だけ取得するの意味。
+    # all()だと、結果を複数リスト形式で取得する。
+    content = WikiContent.query.filter_by(title=title).first()
+    if content is None:
+        abort(404)
+    # show_content.htmlを表示。引数にcontentを渡す。
+    return render_template("show_content.html", content=content)
+
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
